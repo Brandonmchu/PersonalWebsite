f62cc3d856763dd7e9fdc0a67a3b33cbcaa8d11e
Brandon Chu <brandon@tunezy.com>
2013-05-07 09:45:05 -0400

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325004716) do
+ActiveRecord::Schema.define(:version => 20130402233033) do
+
+  create_table "jobs", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.integer  "plantype"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,11 +27,9 @@ ActiveRecord::Schema.define(:version => 20130325004716) do
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
-    t.string   "remember_token"
     t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
