7e2791a5ef99852829e7a0350ed169c7f140d262
Brandon Chu <brandon@tunezy.com>
2013-05-08 18:39:39 -0400

    Styles the homepage

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -8,5 +8,55 @@ $(document).ready(function(){
     });
   });
 
+  //hoverstate
+  $('.post-card').mouseenter(function(){
+    jQuery(this).find(".overlay").css("opacity","0.2");
+    jQuery(this).find("h1").css("text-shadow","-1px 0 1px #333, 0 1px 1px #333, 1px 0 1px #333, 0 -1px 1px #333");
+    jQuery(this).find("h2").css("text-shadow","-1px 0 1px #333, 0 1px 1px #333, 1px 0 1px #333, 0 -1px 1px #333");
+  });
+
+  $('.post-card').mouseleave(function(){
+    jQuery(this).find(".overlay").css("opacity","0.6");
+    jQuery(this).find("h1").css("text-shadow","none");
+    jQuery(this).find("h2").css("text-shadow","none");
+  });
+
+  //spaceman
+  function voyage(){
+    $('.hero-inner img').animate({
+    top: '+=100',
+    left: '+=80'
+    }, 10000,'linear', function(){
+        $(this).animate({
+        top: '-=100',
+        left: '+=180'
+      }, 12000,'linear', function(){
+          $(this).animate({
+          top: '-=100',
+          left: '-=90'
+        }, 8000,'linear', returnVoyage());
+      });
+    });
+
+  }
+
+  function returnVoyage(){
+    $('.hero-inner img').animate({
+    top: '+=100',
+    left: '+=90'
+    }, 10000,'linear', function(){
+      $(this).animate({
+      top: '+=100',
+      left: '-=180'
+      }, 12000,'linear', function(){
+        $(this).animate({
+        top: '-=100',
+        left: '-=80'
+        }, 8000,'linear',voyage());
+      });
+    });
+  }
+  voyage();
+  //end spaceman
 });
 
