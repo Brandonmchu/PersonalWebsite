ff3bec743e14acb245d45d9f41e8171bcd96da46
Brandon Chu <brandon@tunezy.com>
2013-05-15 12:57:04 -0400

    adds precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ BCapp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.precompile += %w( *.js *.css )
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
